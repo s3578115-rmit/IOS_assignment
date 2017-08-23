39ef10967c2dbc3a48dcd78698e86f90a776f93c
niraj2695 <nirajbohra@gmail.com>
2017-08-23 18:13:11 +1000

    TableView update

---
+++ Revents/EventController.swift
@@ -0,0 +1,19 @@
+ //
+//  EventController.swift
+//  Revents
+//
+//  Created by Niraj  Bohra on 8/23/17.
+//  Copyright © 2017 Niraj  Bohra. All rights reserved.
+
+ import UIKit
+ 
+ class EventController:UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Tarot List"
+  
+    
+    }
+
+ }
